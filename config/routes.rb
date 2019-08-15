68d68bf8e4a0e032157960b1b7ce4bd294de0cc7
Lila Liu <lilayliu@gmail.com>
2019-08-15 16:37:47 +0100

    resigned show page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [:new, :create, :show, :destroy]
+    resources :doses, only: [:new, :create, :show]
   end
-
+  resources :doses, only: [:destroy]
   root 'cocktails#index'
 end
