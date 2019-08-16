e0fc7071f988e212c5cbe2b87f514b64c4433538
Lila Liu <lilayliu@gmail.com>
2019-08-16 18:18:07 +0100

    removed destroy all from seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
 require 'open-uri'
 require 'json'
 
-Dose.destroy_all
-Ingredient.destroy_all
-
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
