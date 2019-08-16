485a2af61db0e9fcd255e004f51e219786d896e0
Lila Liu <lilayliu@gmail.com>
2019-08-16 16:44:29 +0100

    added api for ingredient droplist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,18 @@
+require 'open-uri'
+require 'json'
+
 Dose.destroy_all
 Ingredient.destroy_all
 
-Ingredient.create(name: 'lime')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'sugar')
-Ingredient.create(name: 'mint')
-Ingredient.create(name: 'white rum')
-Ingredient.create(name: 'soda water')
-Ingredient.create(name: 'orange')
-Ingredient.create(name: 'pear')
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'strawberry')
-Ingredient.create(name: 'cinamon')
-Ingredient.create(name: 'red wine')
-Ingredient.create(name: 'brandy')
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+drink_list = ingredients["drinks"]
+drink_list.each do |drink|
+ Ingredient.create(name: drink["strIngredient1"])
+end
 
 Cocktail.create(name: 'Mojito')
 Cocktail.create(name: 'Whisky Sour')
 Cocktail.create(name: 'Sex on the beach')
 
-# Dose.create!(description: 'one', cocktail_id: 5, ingredient_id: 6)
